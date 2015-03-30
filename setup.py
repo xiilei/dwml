54a1a741554e44993c01f3bb19fe61d8a5aa9869
empty125 <xilei125@163.com>
2015-03-30 16:02:30 +0800

    Update the setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	name='dwml',
 	version=__version__,
 	license='http://www.apache.org/licenses/LICENSE-2.0',
-	description='read drawing of Ms-office (2007 and above,omml,picture),And Convert them to latex',
+	description='reading DrawingML of Ms-office (2007 and above,omml,picture),And Convert them to latex',
 	author='xilei',
 	author_email='xilei125@163.com',
 	packages=['dwml'],
@@ -17,12 +17,12 @@ setup(
 	classifiers=[
 		'Development Status :: 3 - Alpha Development Status',
 		'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Office/Education',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: Implementation :: CPython',
+		'Programming Language :: Python :: Implementation :: PyPy',
+		'Topic :: Office/Education',
 	]
 
 )
